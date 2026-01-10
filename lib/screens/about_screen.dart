180f77603de221a9cfe0541f2e1d122f99cd9b46
Maksaline <msajib221134@bscse.uiu.ac.bd>
2026-01-10 23:59:44 +0600

    New splash and launcher icon...

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -43,7 +43,7 @@ class AboutScreen extends StatelessWidget {
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(16),
                 child: Image.asset(
-                  'assets/logo.png',
+                  'assets/sc.png',
                   width: 120,
                   height: 120,
                   fit: BoxFit.cover,
